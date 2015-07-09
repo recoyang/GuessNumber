c1eae79ab9413435855379fdf69d8c7bd97eef35
Yuetong Yang <recoyang1992@gmail.com>
2015-07-09 13:40:32 +0800

    change Valueable name

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -5,7 +5,7 @@ public class GuessNumber {
 
 	public static void main(String[] args) {
 		
-		int[] a = AnswerGenerator.generateRandomNumber();
+		int[] answer = AnswerGenerator.generateRandomNumber();
 		int gameTime = 6;
 	
 		Scanner scr = new Scanner(System.in);
@@ -18,12 +18,12 @@ public class GuessNumber {
 			System.out.println("Please enter your number("+gameTime+")");
 			int userInputNum = scr.nextInt();
 			int[] userInput = new int[4];
-			for (int i = 0; i < userInput.length; i++) {
+			for (int i = userInput.length-1; i >= 0 ; i--) {
 				userInput[i] = userInputNum%10;
 				userInputNum /= 10;
 			}
 			
-			String result =	CompareNumber.compareNumbers(userInput,a);
+			String result =	CompareNumber.compareNumbers(userInput,answer);
 			
 			if (result.equals("4A0B")) {
 				answerStatus = "Congratulations";
